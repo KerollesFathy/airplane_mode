5c05d045623aa37675a3f88eee0b306c02068111
KerollesFathy <kerolles.f@outlook.com>
2023-11-25 16:42:33 +0200

    Saving Flight Passenger throws an exception when last name is not set

--- airplane_mode/airplane_mode/doctype/flight_passenger/flight_passenger.py
+++ airplane_mode/airplane_mode/doctype/flight_passenger/flight_passenger.py
@@ -1,9 +1,13 @@
 # Copyright (c) 2023, k.fathy@axentor.co and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class FlightPassenger(Document):
 	def before_save(self):
 		self.full_name = self.first_name + " " +  self.last_name
+	
+	def validate(self):
+		if not self.last_name:
+			frappe.throw("Please set last name")
