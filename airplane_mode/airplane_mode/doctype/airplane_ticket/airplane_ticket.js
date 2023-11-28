0cd982f27fee8625f8534cf331d084f866ff6ae1
KerollesFathy <kerolles.f@outlook.com>
2023-11-28 21:49:07 +0200

    add Assign seat button

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
@@ -1,8 +1,19 @@
 // Copyright (c) 2023, k.fathy@axentor.co and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Airplane Ticket', {
-	// refresh: function(frm) {
-
-	// }
+frappe.ui.form.on("Airplane Ticket", {
+	refresh: function(frm) {
+		frm.add_custom_button("Assign Seat", () => {
+			frappe.prompt([
+					{"fieldname": "seat_no", "fieldtype": "Data", "label": "Seat Number", "reqd": 1}
+				],
+				function(values){
+					frm.set_value("seat", values.seat_no);
+					frm.save();
+				},
+				"Select Seat",
+				"Assign"
+			)
+		} , "Actions")
+	}
 });
