c7c92941a9c0f53c933db16873236a25aa2b6b72
KerollesFathy <kerolles.f@outlook.com>
2023-11-30 18:36:36 +0200

    added unit tests

--- airplane_mode/airplane_mode/doctype/airplane_flight/test_airplane_flight.py
+++ airplane_mode/airplane_mode/doctype/airplane_flight/test_airplane_flight.py
@@ -1,9 +1,32 @@
 # Copyright (c) 2023, k.fathy@axentor.co and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 from frappe.tests.utils import FrappeTestCase
-
+import unittest
+from frappe.website.website_generator import WebsiteGenerator
+from unittest.mock import MagicMock
+from airplane_mode.airplane_mode.doctype.airplane_flight.airplane_flight import AirplaneFlight
 
 class TestAirplaneFlight(FrappeTestCase):
-	pass
+    def setUp(self):
+        # Create a test document before each test
+        self.flight = frappe.get_doc({
+            "doctype": "Airplane Flight",
+            "status": "Scheduled",
+        }).insert(ignore_permissions=True, ignore_mandatory=True)
+
+        
+    def tearDown(self):
+        # Delete the test document after each test
+        frappe.delete_doc("Airplane Flight", self.flight.name, force=True)
+
+    def test_on_submit_changes_status(self):
+        # Ensure that on_submit changes the status to "Completed"
+        self.assertEqual(self.flight.status, "Scheduled")
+
+        # Call on_submit method
+        self.flight.on_submit()
+
+        # Check if the status is updated
+        self.assertEqual(self.flight.status, "Completed")
--- airplane_mode/airplane_mode/doctype/flight_passenger/test_flight_passenger.py
+++ airplane_mode/airplane_mode/doctype/flight_passenger/test_flight_passenger.py
@@ -1,9 +1,32 @@
 # Copyright (c) 2023, k.fathy@axentor.co and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 from frappe.tests.utils import FrappeTestCase
 
 
 class TestFlightPassenger(FrappeTestCase):
-	pass
+       
+	def setUp(self):
+        # Create a test instance of FlightPassenger
+		self.flight_passenger = frappe.get_doc({
+			"doctype": "Flight Passenger",
+			"first_name": "John",
+			"last_name": "Doe"
+		})
+
+	def tearDown(self):
+		# Delete the test document after each test
+		if self.flight_passenger.get("name"):
+			frappe.delete_doc("Flight Passenger", self.flight_passenger.name, force=True)
+
+	def test_before_save_concatenates_names(self):
+		# Ensure that before_save concatenates first and last names
+		self.assertEqual(self.flight_passenger.full_name, None)
+
+		# Call before_save method
+		self.flight_passenger.before_save()
+
+		# Check if the full_name is concatenated
+		self.assertEqual(self.flight_passenger.full_name, "John Doe")
+
